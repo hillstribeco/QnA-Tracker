2176a84a759b6cc7d14ce0ba21147c4779948bee
hillstribeco <hillstribeco@gmail.com>
2026-06-04 11:56:05 +0200

    Delete DE QnA Tracker directory

--- DE QnA Tracker/public/js/auth.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  function normalizeEmail(email) { return String(email || '').trim().toLowerCase(); }
-  function isValidEmail(email) { return emailPattern.test(normalizeEmail(email)); }
-  window.AppAuth = { normalizeEmail, isValidEmail };
-})();
